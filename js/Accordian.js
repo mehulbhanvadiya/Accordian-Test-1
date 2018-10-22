9e80863eb6a9c6c4dbe0a583db278ef6ef7a71c5
Mehul Bhanvadiya <csmehul22@gmail.com>
2018-10-22 23:50:14 +0530

    files add

---
+++ js/Accordian.js
@@ -0,0 +1,16 @@
+
+$('.accordion .item .heading').click(function() {
+		
+	var a = $(this).closest('.item');
+	var b = $(a).hasClass('open');
+	var c = $(a).closest('.accordion').find('.open');
+		
+	if(b != true) {
+		$(c).find('.content').slideUp(500);
+		$(c).removeClass('open');
+	}
+
+	$(a).toggleClass('open');
+	$(a).find('.content').slideToggle(500);
+
+});
